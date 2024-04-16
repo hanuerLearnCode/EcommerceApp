3e86a55f747d36e3262d1c0a5ef6197e599eb9e8
imoutstagram <imoutstagram@gmail.com>
2024-04-16 11:18:55 +0700

    feature: add phone & address to user information, update the registration flow

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,7 +14,6 @@
         <!-- Scripts -->
 {{--        @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body>
